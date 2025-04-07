597356d464e18889e975eb881425ad6bd09a6941
NkwiCyril <akinimbomnkwi@gmail.com>
2025-04-07 20:34:09 +0100

    feat: integrate Laravel Sanctum for API authentication

--- banking-api/bootstrap/app.php
+++ banking-api/bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'api_key' => \App\Http\Middleware\ApiKeyMiddleware::class,
+            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
