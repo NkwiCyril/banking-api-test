0a911c227ac7cc27202cd41f815a8fd4a410a72b
NkwiCyril <akinimbomnkwi@gmail.com>
2025-04-08 10:25:48 +0100

    refactor: middleware implentation on API routes

--- banking-api/routes/api.php
+++ banking-api/routes/api.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('api_key')->group(function () {
+Route::middleware(['auth:sanctum', 'api_key'])->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware(['auth:sanctum', 'role:admin,employee'])->group(function () {
+    Route::middleware('role:admin,employee')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::apiResource('customers', CustomerController::class);
         Route::apiResource('accounts', BankAccountController::class)->only(['store']);
